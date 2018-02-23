9806740ef5acf1e444fb3b1e80f7545ff41070e0
WillB01 <wb.william@outlook.com>
2018-02-23 12:34:15 +0100

    Adding Stuff

--- ProjektuppgiftAspDotNet/Startup.cs
+++ ProjektuppgiftAspDotNet/Startup.cs
@@ -5,26 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ProjektuppgiftAspDotNet
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+       
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+                routes.MapRoute(
+                    name: "default",
+                    template: "{Controller=Home}/{action=Index}/{id?}"
+            ));
 
 
            
