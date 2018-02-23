a4fde08b00c3cefc2de576c4889052499d27e945
WillB01 <wb.william@outlook.com>
2018-02-23 12:41:15 +0100

    Adding Stuff

---
+++ ProjektuppgiftAspDotNet/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjektuppgiftAspDotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektuppgiftAspDotNet.Interface
+{
+    public interface IUserRepository
+    {
+        IQueryable<User> Users { get; set; }
+    }
+}
---
+++ ProjektuppgiftAspDotNet/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektuppgiftAspDotNet.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
