1750b9165db3ed117b79ef6f076ec5bb6813966e
WillB01 <wb.william@outlook.com>
2018-02-24 01:24:42 +0100

    kewl

--- ProjektuppgiftAspDotNet/Controllers/HomeController.cs
+++ ProjektuppgiftAspDotNet/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace ProjektuppgiftAspDotNet.Controllers
     public class HomeController : Controller
     {
         private readonly IUserRepository _userRepository;
+
         public HomeController(IUserRepository userRepository)
         {
             _userRepository = userRepository;
