e484b45a02d62301baf3b68dd5d7ab310a8bd2ed
WillB01 <wb.william@outlook.com>
2018-02-27 12:53:31 +0100

    added ViewComponent

---
+++ ProjektuppgiftAspDotNet/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektuppgiftAspDotNet.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektuppgiftAspDotNet.ViewComponents
+{
+    public class UserViewComponent : ViewComponent
+    {
+        private IUserRepository _userRepository;
+
+        public UserViewComponent(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _userRepository.GetUser.OrderByDescending(u => u.Id);
+            return View("Default", model);
+        }
+    }
+}
