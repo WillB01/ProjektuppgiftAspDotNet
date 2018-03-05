39c15d86acf25d7fbf63c27c60a0e4fb6080d215
WillB01 <wb.william@outlook.com>
2018-03-05 12:56:36 +0100

    adding

---
+++ ProjektuppgiftAspDotNet/ViewComponents/UserSearchBarViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektuppgiftAspDotNet.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektuppgiftAspDotNet.ViewComponents
+{
+    public class UserSearchBarViewComponent : ViewComponent
+    {
+        private IUserIdentityRepository _userIdentityRepository;
+
+        public UserSearchBarViewComponent(IUserIdentityRepository userIdentityRepository)
+        {
+            _userIdentityRepository = userIdentityRepository;
+        }
+
+        [HttpPost]
+        public IViewComponentResult Invoke(string Prefix)
+        {
+            var Countries = (from c in _userIdentityRepository.GetAppUser
+                             where c.UserName.StartsWith(Prefix)
+                             select new { value = c.UserName });
+
+
+            return View("Default", Countries);
+        }
+
+
+
+    }
+}
+
